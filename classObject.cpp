4716b536a4bad6929732ed4bb0344952e1f731a5
amaankhanakz <sameeramaan2014@gmail.com>
2023-08-29 11:44:59 +0530

    start

---
+++ classObject.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// class
+class Hero
+{
+  // properties
+  // char name[100];
+  int health;
+  // char level;
+};
+
+int main()
+{
+  // creation of object
+  Hero h1;
+
+  cout << "Size: " << sizeof(h1) << endl;
+
+  return 0;
+}
